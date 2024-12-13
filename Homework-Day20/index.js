3c01401513479a9b517088fff6f4b5ca8cfc7940
minhdang0 <dangnhatminh8402@gmail.com>
2024-12-13 13:42:26 +0700

    fix

--- Homework-Day20/index.js
+++ Homework-Day20/index.js
@@ -1,11 +1,3 @@
-const students = [
-    { name: 'Nguyen Van A', score: 9 },
-    { name: 'Tran Thi B', score: 7 },
-    { name: 'Le Van C', score: 5 },
-    { name: 'Pham Thi D', score: 3 },
-    { name: 'Doan Van E', score: 10 },
-    { name: 'Hoang Thi F', score: 6 },
-];
 function groupStudent(students) {
     let highest = students[0];
     let lowest = students[0];
@@ -43,6 +35,14 @@ function groupStudent(students) {
     groupByStudent.group = group;
     return groupByStudent;
 }
+const students = [
+    { name: 'Nguyen Van A', score: 9 },
+    { name: 'Tran Thi B', score: 7 },
+    { name: 'Le Van C', score: 5 },
+    { name: 'Pham Thi D', score: 3 },
+    { name: 'Doan Van E', score: 10 },
+    { name: 'Hoang Thi F', score: 6 },
+];
 console.log(groupStudent(students));
 
 
