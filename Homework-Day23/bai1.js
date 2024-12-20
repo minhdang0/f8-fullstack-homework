75a27d3c1ef9f70d89b01c462f8657f10b806cca
minhdang0 <dangnhatminh8402@gmail.com>
2024-12-20 13:05:19 +0700

    push

---
+++ Homework-Day23/bai1.js
@@ -0,0 +1,57 @@
+const products = [
+	{
+		id: 1,
+		name: "Apple iPhone 12",
+		price: 1000,
+	},
+	{
+		id: 2,
+		name: "Samsung Galaxy S21",
+		price: 900,
+	},
+	{
+		id: 3,
+		name: "Xiaomi Mi 11",
+		price: 800,
+	},
+	{
+		id: 4,
+		name: "Apple iPhone 11",
+		price: 700,
+	},
+	{
+		id: 5,
+		name: "Samsung Galaxy S20",
+		price: 600,
+	},
+	{
+		id: 6,
+		name: "Xiaomi Mi 10",
+		price: 500,
+	},
+];
+
+const getTopProducts = (products, top) => {
+    const topProduct = [];
+
+    while (topProduct.length < top){
+        
+        let maxIndex = 0; 
+
+        for ( let i = 1; i < products.length; i++){
+            if (products[i].price > products[maxIndex].price) {
+                maxIndex = i;
+            }
+        }
+
+        topProduct.push(products[maxIndex]);
+
+        products.splice(maxIndex,1)
+    }
+
+    return topProduct;
+
+}
+
+const result = getTopProducts(products, 3);
+console.log(result);
---
+++ Homework-Day23/bai2.js
@@ -0,0 +1,80 @@
+const categories = [
+	{
+		id: 1,
+		name: "Electronics",
+		children: [
+			{
+				id: 2,
+				name: "Laptops",
+				children: [
+					{
+						id: 3,
+						name: "Apple",
+					},
+					{
+						id: 4,
+						name: "Dell",
+					},
+				],
+			},
+			{
+				id: 5,
+				name: "Headphones",
+			},
+		],
+	},
+	{
+		id: 6,
+		name: "Books",
+		children: [
+			{
+				id: 7,
+				name: "Fiction",
+				children: [
+					{
+						id: 8,
+						name: "Thrillers",
+					},
+					{
+						id: 9,
+						name: "Mystery",
+					},
+				],
+			},
+			{
+				id: 10,
+				name: "Non-Fiction",
+			},
+		],
+	},
+];
+
+const flattenCategories = (categories, level = 0) => {
+    let result = [];
+
+    for (let i = 0 ; i < categories.length ; i++) {
+        const {id, name} = categories[i];
+        result.push({id,name,level});
+
+        if(categories[i].children) {
+            result = result.concat(flattenCategories(categories[i].children, level + 1 ))
+        }
+    }
+
+    return result;
+}
+
+const newArray = flattenCategories(categories);
+console.log(newArray);
+
+const getCategoryName = (categories, categoryId) =>{
+    const result = categories.find((categories) => categories.id === categoryId);
+
+    if (!result) return null;
+
+    return result.name;
+}
+
+console.log(getCategoryName(newArray, 3)); // Output: "Apple"
+console.log(getCategoryName(newArray, 10)); // Output: "Non-Fiction"
+console.log(getCategoryName(newArray, 99)); // Output: null
